aca9177d016598fee19156a68a8d91eb4b5787fb
Erik <erik.tran.tt@gmail.com>
2020-08-22 17:15:50 +1000

    initial commit, completed part 2 to setup routing paths and responses

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -1,12 +1,35 @@
-class BotController < Stealth::Controller
+# frozen_string_literal: true
 
+class BotController < Stealth::Controller
   helper :all
 
   def route
+    if current_message.payload.present?
+      handle_payloads
+      current_message.payload = nil
+      return
+    end
+
     if current_session.present?
       step_to session: current_session
     else
       step_to flow: 'hello', state: 'say_hello'
+    end
+
+  end
+
+  private
+
+  def handle_payloads
+    payload = current_message.payload
+    restart_buttons = %w[restart\ pressed restart_button dev_button].freeze
+
+    if payload == 'new_user'
+      step_to flow: 'hello', state: 'say_hello'
+    elsif restart_buttons.include?(payload)
+      step_to flow: 'goodbye', state: 'say_hello_again'
+    else
+      return
     end
   end
 
